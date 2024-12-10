f8263ac28051e5dbd2fd229fc6388eff307f576c
Amal Sajeev <amsajeev333@gmail.com>
2024-12-10 13:59:27 +0530

    Basic Practice demo ui ready.

---
+++ practice.py
@@ -0,0 +1,73 @@
+import streamlit as st
+import wizard
+from streamlit_cookies_controller import CookieController
+import time
+from datetime import datetime
+import functions
+
+if st.session_state.current_session['dynamic'] == True:
+    st.title(f"Executing Dynamic Assessment:")
+else:
+    st.title("Executing Static Assessment:")
+
+testwizard = wizard.LearningPlatformSDK("http://localhost:8100")
+controller = CookieController() 
+ 
+cookies = controller.getAll() 
+client = "testertest" 
+if st.session_state.current_session:
+    with st.sidebar:
+        st.title("Current User")
+        st.code(st.session_state.user_id)
+        if "_id" in st.session_state.current_session:
+            st.write("Current session:")
+            st.code(st.session_state.current_session['_id'])
+            st.header(":black-background[PRACTICE SESSION]")
+
+def dynamic_practice():
+    if 'current_question' not in st.session_state:
+        st.session_state.current_question = testwizard.start_practice(client, st.session_state.current_session['_id'])
+    if "message" not in st.session_state.current_question:
+        st.write(st.session_state.current_question)
+        st.write(st.session_state.current_question['question_content'])
+        
+        selection = st.radio(
+            "Select your answer", 
+            options=st.session_state.current_question['question_options'].values(), 
+            index=None, 
+            key="question_selection"  
+        )
+        dif_options = ["Easy", "Medium", "Hard", "Again"]
+        dif_selection = st.radio(
+            "How difficult was it to remember the answer?",
+            options = dif_options, index= None
+        )
+        
+        
+        if st.button("Next", key="next_question_btn"):
+            if selection is not None:
+                
+                selected_index = list(st.session_state.current_question['question_options'].keys())[list(st.session_state.current_question['question_options'].values()).index(selection)]
+
+                sel_dif_index = dif_options.index(dif_selection)
+
+                # Proceed to the next question
+                st.session_state.current_question = testwizard.continue_practice(
+                    st.session_state.current_session['_id'], 
+                    st.session_state.current_question['_id'], 
+                    selected_index
+                )
+                st.rerun()
+            else:
+                st.warning("Please select an answer before proceeding.")
+    else:
+        st.balloons()
+        functions.results(st.session_state.current_question)
+
+
+with st.container(border=True):
+    if st.session_state.current_session['dynamic'] == True:
+        dynamic_assessment()
+    else:
+        st.write(static_assessment())
+        
--- streamtest.py
+++ streamtest.py
@@ -84,22 +84,29 @@ if st.session_state.user_id:
                     st.write(datetime.fromisoformat(i['created']).time())
                     st.write(f"Assigned: {i['created']}")
                     st.write(f"Score: {i['total_score']}/{i['max_score']}")
-                    st.write(":rainbow[Dynamic]" if i['dynamic'] == True else ":grey-background[Static]")
-                    if i['total_score'] < i['max_score']:
-                        if i['status'] == "Started":
-                            if st.button("Continue Assessment", key=i['_id']+"1"):
-                                controller.set("current_session", i)
-                                st.session_state.current_session = controller.get("current_session") 
-                                if "current_question" in st.session_state:
-                                    st.session_state.pop("current_question")
-                                st.switch_page("pages/assessment.py")
-                        else:    
-                            if st.button("Start Assessment", key=i['_id']+"1"):
-                                controller.set("current_session", i)
-                                st.session_state.current_session = controller.get("current_session") 
-                                if "current_question" in st.session_state:
-                                    st.session_state.pop("current_question")
-                                st.switch_page("pages/assessment.py")
+                    st.write(":rainbow-background[Dynamic]" if i['dynamic'] == True else ":grey-background[Static]")
+                    if i['status'] == "Started":
+                        if st.button("Continue Assessment", key=i['_id']+"1"):
+                            controller.set("current_session", i)
+                            st.session_state.current_session = controller.get("current_session") 
+                            if "current_question" in st.session_state:
+                                st.session_state.pop("current_question")
+                            st.switch_page("pages/assessment.py")
+                    elif i['status'] == "Finished":
+                        if  i["dynamic"] != True:
+                            st.write(f"Average: {i['score_average']}")
+                        else:
+                            st.write(f"Relative Score: {i['relative_score']}")
+                            st.write(f"Efficiency Score: {i['efficiency_score']}")
+                            st.write(f"Performance Score: {i['performance_score']}")
+                            
+                    else:    
+                        if st.button("Start Assessment", key=i['_id']+"1"):
+                            controller.set("current_session", i)
+                            st.session_state.current_session = controller.get("current_session") 
+                            if "current_question" in st.session_state:
+                                st.session_state.pop("current_question")
+                            st.switch_page("pages/assessment.py")
                     if st.button("Delete assignment", key=f"delete {i['_id']}"):
                         st.toast(testwizard.delete_assessment(client,i['_id']))
                         st.rerun()
